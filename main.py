b4cfa5048e9457c675b54feaa8c76b25dcf79e86
EdwardProm <eduardkoles@gmail.com>
2024-05-13 19:33:58 +0300

    multiprocessing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 from pathlib import Path
 from urllib.parse import urlparse, unquote_plus
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from threading import Thread
+from multiprocessing import Process
 
 from jinja2 import Environment, FileSystemLoader
 from pymongo.mongo_client import MongoClient
@@ -82,7 +82,7 @@ class TheBestFramework(BaseHTTPRequestHandler):
 
 
 def run_http_server():
-    httpd = HTTPServer((HTTP_HOST, HTTP_PORT), TheBestFramework)  # noqa
+    httpd = HTTPServer((HTTP_HOST, HTTP_PORT), TheBestFramework)
     try:
         logging.info(f"Server started: http://{HTTP_HOST}:{HTTP_PORT}")
         httpd.serve_forever()
@@ -96,7 +96,6 @@ def run_http_server():
 def save_to_db(data):
     client = MongoClient(URI_DB, server_api=ServerApi('1'))
     db = client.homework
-    # name=Krabat&email=krabat@i.ua&text=Hello  -> {"name": "Krabat", "email": "krabat@i.ua", "text": "Hello "}
     try:
         data = unquote_plus(data)
         parse_data = dict([i.split("=") for i in data.split("&")])
@@ -125,6 +124,10 @@ def run_socket_server():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(threadName)s - %(message)s")
-    Thread(target=run_http_server, name="HTTP_Server").start()
-    Thread(target=run_socket_server, name="SOCKET_Server").start()
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(processName)s - %(message)s")
+    http_server_process = Process(target=run_http_server, name="HTTP_Server")
+    socket_server_process = Process(target=run_socket_server, name="SOCKET_Server")
+    http_server_process.start()
+    socket_server_process.start()
+    http_server_process.join()
+    socket_server_process.join()
